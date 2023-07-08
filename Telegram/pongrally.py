6497d11a4ba922317aefce740891373be1e8ffec
Khulon <88884346+Khulon@users.noreply.github.com>
2023-07-08 16:31:41 +0800

    added inline when type @pongrallybot

--- Telegram/pongrally.py
+++ Telegram/pongrally.py
@@ -2,7 +2,6 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils.exceptions import InvalidQueryID
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -28,25 +27,29 @@ async def play_game(callback_query: types.CallbackQuery):
     # Open the game URL in the user's browser
     await bot.answer_callback_query(callback_query.id, url="https://64a16caa22c0774d8d878959--khulonpong.netlify.app/")
 
-# Define the command handler for initiating the conversation
-@dp.message_handler(commands=['start_game'])
-@dp.message_handler(lambda message: message.text == START_PHRASE)
-async def start(message: types.Message):
-    # Generate InlineQueryResultGame
-    game_result = types.InlineQueryResultGame(
-        id='1',
-        game_short_name=GAME_SHORT_NAME,
-        reply_markup=types.InlineKeyboardMarkup(inline_keyboard=[
-            [types.InlineKeyboardButton(text="Play Game Name", callback_game=GAME_SHORT_NAME)]
-        ])
-    )
 
-    try:
+# Define the command handler for initiating the conversation
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    chat_id = message.chat.id
+    # Send a message with the game and a "Play Game" button
+    await bot.send_game(chat_id, game_short_name=GAME_SHORT_NAME)
+
+# Define the inline query handler for handling inline queries
+@dp.inline_handler()
+async def inline_query_handler(inline_query: types.InlineQuery):
+    if inline_query.query == '':
+
+        game_result = types.InlineQueryResultGame(
+            id='1',
+            game_short_name=GAME_SHORT_NAME,
+            reply_markup=types.InlineKeyboardMarkup(inline_keyboard=[
+                [types.InlineKeyboardButton(text="Play Game!", callback_game=GAME_SHORT_NAME)]
+            ]),
+        )
+        
         # Answer the inline query with the game result
-        await bot.answer_inline_query(message.chat.id, results=[game_result])
-    except InvalidQueryID:
-        # Handle the InvalidQueryID exception
-        pass
+        await bot.answer_inline_query(inline_query.id, results=[game_result])
 
 # Define the callback query handler for handling game button clicks
 @dp.callback_query_handler(lambda c: c.game_short_name == GAME_SHORT_NAME)
@@ -58,3 +61,14 @@ async def main():
     await dp.start_polling()
 
 asyncio.run(main())
+
+
+
+
+
+
+
+
+
+
+
